bda2ef6d3fc2df1ffa41c68f08ae415ff9a44c77
ANANYA542 <ananyashrma6512@gmail.com>
2025-10-09 13:56:33 +0530

    Update outputcard.jsx

--- frontend/src/component/outputcard.jsx
+++ frontend/src/component/outputcard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import "./OutputCard.css";
+import "./Outputcard.css";
 
 const OutputCard = () => {
   const location = useLocation();
